bcc2e963ce5c529a2e6006f397213c05849daac1
Bruno Fukumori <brunofukumori@gmail.com>
2022-03-21 15:49:55 -0300

    fix links typo

--- src/components/RepositoryItem.tsx
+++ src/components/RepositoryItem.tsx
@@ -11,7 +11,7 @@ export function RepositoryItem(props: RepositoryItemProps) {
     <li>
       <strong>{props.repository.name}</strong>
       <p>{props.repository.description}</p>
-      <a href="{props.repository.html_url}">Ir para o reppsitório</a>
+      <a href={props.repository.html_url}>Ir para o repositório</a>
     </li>
   )
 }
